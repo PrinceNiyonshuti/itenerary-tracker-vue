0efc701961d20ba7a61815330edd5a261a88006d
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-14 21:44:43 +0200

    created Header component and passed props

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<!-- @format -->
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
