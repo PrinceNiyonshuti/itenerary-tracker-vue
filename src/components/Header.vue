7e83ba0a190d361a8b3bc0c8826b90b7cd554486
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-16 14:20:53 +0200

    Restrict a Component to a Route and create views

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,21 +3,34 @@
 <template>
 	<header>
 		<h1>{{ title }}</h1>
-        <Button :text="showAddTask? 'Close' : 'Add Task' " :color="showAddTask? 'red':'green' " @toggle-add-task="$emit('toggle-add-task')"/>
+		<Button
+			v-show="homePage"
+			:text="showAddTask ? 'Close' : 'Add Task'"
+			:color="showAddTask ? 'red' : 'green'"
+			@toggle-add-task="$emit('toggle-add-task')"
+		/>
 	</header>
 </template>
 <script>
-import Button from './Button.vue';
-
+	import Button from "./Button.vue";
 	export default {
 		name: "Header",
 		props: {
 			title: String,
-            showAddTask:Boolean,
+			showAddTask: Boolean,
 		},
-        components:{
-            Button
-        }
+		components: {
+			Button,
+		},
+		computed: {
+			homePage() {
+				if (this.$route.path === "/") {
+					return true;
+				} else {
+					return false;
+				}
+			},
+		},
 	};
 </script>
 <style scoped>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,15 @@
+<!-- @format -->
+
 <template>
-    <h3>About The App </h3>
-    <p>Version 1.0.0</p>
-    <router-link to='/'>Home</router-link>
+	<h3>About The App</h3>
+	<p>Version 1.0.0</p>
+	<router-link to="/">Home</router-link>
 </template>
 <script>
-export default {
-    name:'About'
-}
+	export default {
+		name: "About",
+		props: {
+			showAddTask: Boolean,
+		},
+	};
 </script>
